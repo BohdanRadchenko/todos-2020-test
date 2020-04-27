6f376fb5efa1c6a9b4660f50c5055e20b605f658
BohdanRadchenko <radchenkobs@gmail.com>
2020-04-27 17:08:17 +0300

    initialy app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const config = require('config')
+
+const PORT = process.env.PORT || config.get("port") || 5000
+
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, "build")))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
+const start = async () => {
+        app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
+    } catch (e) {
+        process.exit(1)
+    }
+}
+
+start()
